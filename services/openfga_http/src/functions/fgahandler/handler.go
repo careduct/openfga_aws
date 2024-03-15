e0d46c6cfc1b75f79d7007492e4390ba660de502
Careduct <github@careduct.com>
2024-03-15 17:12:55 +0000

    corrected bug on the conectivity check

--- services/openfga_http/src/functions/fgahandler/handler.go
+++ services/openfga_http/src/functions/fgahandler/handler.go
@@ -23,7 +23,43 @@ type Model struct {
 	FieldTwo string `json:"fieldTwo"`
 }
 
-func waitForOpenfga() {
+func waitForHTTPServer(url string, timeout time.Duration) bool {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	ticker := time.NewTicker(1 * time.Second) // Retry every second
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			// Timeout reached without receiving a 200 OK
+			return false
+		case <-ticker.C:
+			// Attempt to make a request
+			req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+			if err != nil {
+				fmt.Println("Error creating request:", err)
+				continue // Try again on the next tick
+			}
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Error making HTTP request:", err)
+				continue // Try again on the next tick
+			}
+			resp.Body.Close() // Close the response body
+
+			if resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusNotFound {
+				// The server responded with 200 OK or 404 NOT FOUND
+				return true
+			}
+		}
+	}
+}
+
+func waitForOpenfga() int {
 	// Define the URL you want to make a request to
 	url := "http://localhost:4000"
 
@@ -35,7 +71,7 @@ func waitForOpenfga() {
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		fmt.Printf("The OpenFGA didn't started within 10s on the cold start: %v\n", err)
-		return
+		return 504
 	}
 
 	// Create an HTTP client and make the request
@@ -43,9 +79,12 @@ func waitForOpenfga() {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("The OpenFGA didn't started within 10s on the cold start: %v\n", err)
-		return
+		return 504
 	}
-	defer resp.Body.Close() // Don't forget to close the response body
+	resp.Body.Close() // Don't forget to close the response body
+
+	//fmt.Printf("Response status code: %d\n", resp.StatusCode)
+	return resp.StatusCode
 }
 
 func main() {
@@ -55,6 +94,14 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		//wait until the cold start ends if we are in one
+		/*if waitForHTTPServer("http://localhost:4000", 10*time.Second) {
+			fmt.Println("The server is up and running, and responded with 200/404 OK!")
+		} else {
+			fmt.Println("The server did not respond with 200 OK within the specified timeout.")
+			panic("Error instantiating the backend openfga on the lambda extension")
+		}
+		*/
 
 		//create teh reverse çroxy
 		proxy := httputil.NewSingleHostReverseProxy(origin)
